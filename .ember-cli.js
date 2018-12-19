aeaff72ec830603716a55ba0727f1f23838d4604
Jan Buschtöns <jan@buschtoens.me>
2018-12-19 20:46:24 +0100

    reproduction

--- .ember-cli.js
+++ .ember-cli.js
@@ -1,9 +1,14 @@
-{
+/* eslint-env node */
+'use strict';
+
+process.env.EMBER_CLI_MODULE_UNIFICATION = true;
+
+module.exports = {
   /**
     Ember CLI sends analytics information by default. The data is completely
     anonymous, but there are times when you might want to disable this behavior.
 
     Setting `disableAnalytics` to true will prevent any data from being sent.
   */
-  "disableAnalytics": false
-}
+  disableAnalytics: false
+};
